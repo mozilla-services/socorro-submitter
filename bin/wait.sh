72dbba0b595b325e2621444d957588603ac8ae7f
Will Kahn-Greene <willkg@mozilla.com>
2018-04-19 19:14:51 -0400

    Fix docs for wait.sh script

--- bin/wait.sh
+++ bin/wait.sh
@@ -3,12 +3,15 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Waits for 10 seconds for a resource to listen to incoming network connections.
+#
+# Usage: wait.sh HOST PORT
 
 HOST="$1"
 PORT="$2"
 
 TIMEOUT=10
-
 count=0
 
 echo "Connecting to ${HOST}:${PORT}..."
