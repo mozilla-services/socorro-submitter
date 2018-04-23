8596d83caffb46bebbca6d9d2182c9ef6b4c69ee
Will Kahn-Greene <willkg@mozilla.com>
2018-04-23 12:05:26 -0400

    Fix build-libs to use lambda-build container

--- bin/run_build.sh
+++ bin/run_build.sh
@@ -4,7 +4,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Usage: docker-compose run lambda-build ./bin/run_build.sh
+# Run to build Python libraries into the build/ directory.
+#
+# Run this in the lambda-build container using "make build-libs".
 
 # Create the dir if it doesn't exist
 test -d build/ || mkdir build/
