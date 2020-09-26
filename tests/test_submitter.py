40a0ebadc3bdd664e78e3b17253f7ed2d286110e
Will Kahn-Greene <willkg@mozilla.com>
2020-09-26 19:19:52 -0400

    bug 1609739: emit metrics via logging
    We're not using cloudwatch anymore, so we need to emit the metrics in a
    different way. This emits them via logging which we can pick up with
    stackdriver and I can query them in BigQuery.

--- tests/test_submitter.py
+++ tests/test_submitter.py
@@ -2,12 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import logging
+
 import pytest
 
 from submitter import CONFIG, extract_crash_id_from_record
 
 
-def test_basic(client, capsys, fakes3, mock_collector):
+def test_basic(client, caplog, fakes3, mock_collector):
     fakes3.create_bucket()
     fakes3.save_crash(
         raw_crash={
@@ -21,9 +23,11 @@ def test_basic(client, capsys, fakes3, mock_collector):
     crash_id = "de1bb258-cbbf-4589-a673-34f800160918"
     events = client.build_crash_save_events(client.crash_id_to_key(crash_id))
 
-    # Make sure it doesn't get throttled and invoke the Lambda function
-    with CONFIG.override(throttle=100):
-        assert client.run(events) is None
+    # Capture logs, make sure it doesn't get throttled, and invoke the Lambda
+    # function
+    with caplog.at_level(logging.INFO):
+        with CONFIG.override(throttle=100):
+            assert client.run(events) is None
 
     # Verify payload was submitted
     assert len(mock_collector.payloads) == 1
@@ -54,11 +58,10 @@ def test_basic(client, capsys, fakes3, mock_collector):
         "--01659896d5dc42cabd7f3d8a3dcdd3bb--\r\n"
     )
 
-    stdout, stderr = capsys.readouterr()
-    assert "|1|count|socorro.submitter.accept|" in stdout
+    assert "|1|count|socorro.submitter.accept|#env:test" in caplog.record_tuples[0][2]
 
 
-def test_multiple_dumps(client, capsys, fakes3, mock_collector):
+def test_multiple_dumps(client, caplog, fakes3, mock_collector):
     fakes3.create_bucket()
     fakes3.save_crash(
         raw_crash={
@@ -75,9 +78,11 @@ def test_multiple_dumps(client, capsys, fakes3, mock_collector):
     crash_id = "de1bb258-cbbf-4589-a673-34f800160918"
     events = client.build_crash_save_events(client.crash_id_to_key(crash_id))
 
-    # Make sure it doesn't get throttled and invoke the Lambda function
-    with CONFIG.override(throttle=100):
-        assert client.run(events) is None
+    # Capture logs, make sure it doesn't get throttled, and invoke the Lambda
+    # function
+    with caplog.at_level(logging.INFO):
+        with CONFIG.override(throttle=100):
+            assert client.run(events) is None
 
     # Verify payload was submitted
     assert len(mock_collector.payloads) == 1
@@ -114,8 +119,7 @@ def test_multiple_dumps(client, capsys, fakes3, mock_collector):
         "--01659896d5dc42cabd7f3d8a3dcdd3bb--\r\n"
     )
 
-    stdout, stderr = capsys.readouterr()
-    assert "|1|count|socorro.submitter.accept|" in stdout
+    assert "|1|count|socorro.submitter.accept|" in caplog.record_tuples[0][2]
 
 
 def test_non_s3_event_ignored(client, fakes3, mock_collector):
@@ -136,7 +140,7 @@ def test_non_put_event_ignored(client, fakes3, mock_collector):
     assert len(mock_collector.payloads) == 0
 
 
-def test_throttle_accepted(client, capsys, mocker, fakes3, mock_collector):
+def test_throttle_accepted(client, caplog, mocker, fakes3, mock_collector):
     always_20 = mocker.patch("random.randint")
     always_20.return_value = 20
 
@@ -153,17 +157,18 @@ def test_throttle_accepted(client, capsys, mocker, fakes3, mock_collector):
     crash_id = "de1bb258-cbbf-4589-a673-34f800160918"
     events = client.build_crash_save_events(client.crash_id_to_key(crash_id))
 
-    # Set throttle value above the mocked randint, so this should get submitted
-    with CONFIG.override(throttle=30):
-        assert client.run(events) is None
+    # Capture the log and set throttle value above the mocked randint--this should
+    # get submitted
+    with caplog.at_level(logging.INFO):
+        with CONFIG.override(throttle=30):
+            assert client.run(events) is None
 
     # Verify payload was submitted
     assert len(mock_collector.payloads) == 1
-    stdout, stderr = capsys.readouterr()
-    assert "|1|count|socorro.submitter.accept|" in stdout
+    assert "|1|count|socorro.submitter.accept|" in caplog.record_tuples[0][2]
 
 
-def test_throttle_skipped(client, capsys, mocker, fakes3, mock_collector):
+def test_throttle_skipped(client, caplog, mocker, fakes3, mock_collector):
     always_20 = mocker.patch("random.randint")
     always_20.return_value = 20
 
@@ -180,18 +185,19 @@ def test_throttle_skipped(client, capsys, mocker, fakes3, mock_collector):
     crash_id = "de1bb258-cbbf-4589-a673-34f800160918"
     events = client.build_crash_save_events(client.crash_id_to_key(crash_id))
 
-    # Set throttle value below the mocked randint, so this should get skipped
-    with CONFIG.override(throttle=10):
-        assert client.run(events) is None
+    # Capture the logs and set throttle value below the mocked randint--this
+    # should get skipped
+    with caplog.at_level(logging.INFO):
+        with CONFIG.override(throttle=10):
+            assert client.run(events) is None
 
     # Verify no payload was submitted
     assert len(mock_collector.payloads) == 0
 
-    stdout, stderr = capsys.readouterr()
-    assert "|1|count|socorro.submitter.throttled|" in stdout
+    assert "|1|count|socorro.submitter.throttled|" in caplog.record_tuples[1][2]
 
 
-def test_env_tag_added_to_statds_incr(client, capsys, fakes3, mock_collector):
+def test_env_tag_added_to_statds_incr(client, caplog, fakes3, mock_collector):
     fakes3.create_bucket()
     fakes3.save_crash(
         raw_crash={
@@ -202,17 +208,17 @@ def test_env_tag_added_to_statds_incr(client, capsys, fakes3, mock_collector):
         dumps={"upload_file_minidump": "abcdef"},
     )
 
-    with CONFIG.override(env_name="stage", throttle=100):
-        crash_id = "de1bb258-cbbf-4589-a673-34f800160918"
-        #                                        ^ accept
-        events = client.build_crash_save_events(client.crash_id_to_key(crash_id))
-        assert client.run(events) is None
+    with caplog.at_level(logging.INFO):
+        with CONFIG.override(env_name="stage", throttle=100):
+            crash_id = "de1bb258-cbbf-4589-a673-34f800160918"
+            #                                        ^ accept
+            events = client.build_crash_save_events(client.crash_id_to_key(crash_id))
+            assert client.run(events) is None
 
-        # Verify payload was submitted
-        assert len(mock_collector.payloads) == 1
+    # Verify payload was submitted
+    assert len(mock_collector.payloads) == 1
 
-        stdout, stderr = capsys.readouterr()
-        assert "|1|count|socorro.submitter.accept|#env:stage\n" in stdout
+    assert "|1|count|socorro.submitter.accept|#env:stage" in caplog.record_tuples[0][2]
 
 
 @pytest.mark.parametrize(
