87aad7520dbafb5002efafbc3d4edda1c6ced21f
Will Kahn-Greene <willkg@mozilla.com>
2022-09-09 22:52:09 -0400

    Add echo statements to rebuild_reqs.sh for easier debugging

--- bin/rebuild_reqs.sh
+++ bin/rebuild_reqs.sh
@@ -12,11 +12,14 @@
 
 set -euo pipefail
 
+echo ">>> versions...."
 pip --version
 pip-compile --version
 
 # Recompile requirements-dev.txt
+echo ">>> recompile requirements-dev.txt...."
 pip-compile --cache-dir=/tmp --generate-hashes --output-file /app/requirements-dev.txt /app/requirements-dev.in
 
 # Recompile requirements.txt
+echo ">>> recompile requirements.txt...."
 pip-compile --cache-dir=/tmp --generate-hashes --output-file /app/requirements.txt /app/requirements.in
