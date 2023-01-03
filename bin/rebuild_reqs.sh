ad525bb487b99bf17bedccb56e98743c37db62b9
Will Kahn-Greene <willkg@mozilla.com>
2023-01-03 10:43:46 -0500

    bug 1791210: rebuild requirements files
    This rebuilds the requirements files to get around dependency conflicts
    I was having. This also adds `--strip-extras` to the pip-compile line.

--- bin/rebuild_reqs.sh
+++ bin/rebuild_reqs.sh
@@ -18,8 +18,8 @@ pip-compile --version
 
 # Recompile requirements-dev.txt
 echo ">>> recompile requirements-dev.txt...."
-pip-compile --cache-dir=/tmp --generate-hashes --output-file /app/requirements-dev.txt /app/requirements-dev.in
+pip-compile --cache-dir=/tmp --generate-hashes --strip-extras --output-file /app/requirements-dev.txt /app/requirements-dev.in
 
 # Recompile requirements.txt
 echo ">>> recompile requirements.txt...."
-pip-compile --cache-dir=/tmp --generate-hashes --output-file /app/requirements.txt /app/requirements.in
+pip-compile --cache-dir=/tmp --generate-hashes --strip-extras --output-file /app/requirements.txt /app/requirements.in
